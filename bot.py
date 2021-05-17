a2791fabf8dbcf7d1a83c9052982d45d35193b4e
Master-JeyDee <84302496+Master-JeyDee@users.noreply.github.com>
2021-05-17 13:51:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ def start(client, message):
             message.reply_text(text =f"Hello {message.from_user.first_name } \n\n **I am simple YouTube Thumbnail link Generator** \n __Send me Youtube link and get Thumbnail link__",reply_to_message_id = message.message_id , parse_mode="markdown", reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Join 👨‍🦯" ,url="https://t.me/WinInSilenceLetthemthinkURLosing") ],
+                    InlineKeyboardButton("Join 👨‍🦯" ,url="https://t.me/WinInSilenceLetthemthinkURLosing") ]
         ) )
 
 
